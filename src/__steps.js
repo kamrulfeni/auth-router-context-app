9c2ccd89f6f387fd298a2198c585a437a5729465
kamrulHasan <kamrulhasandhaka704@gmail.com>
2023-02-26 07:30:12 +0600

    auth provider context

---
+++ src/__steps.js
@@ -0,0 +1,9 @@
+/**
+ * Basic CONTEXT API setup
+ 1. Context API: Share auth state with other components (across the application)
+ 2.Create an UserContext 
+ 3.ContextProvider with pass children 
+ 4. set the UserContext in the index.js
+ 5.To consume the context : export the AuthContext from userContext 
+ 6.Now at Header or anywhere else : use useContext hook to get the info in the context
+ */
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAemTltAHUc0gi0_am02uaCMIOcrYMFGNE",
+  authDomain: "auth-router-context-d44b4.firebaseapp.com",
+  projectId: "auth-router-context-d44b4",
+  storageBucket: "auth-router-context-d44b4.appspot.com",
+  messagingSenderId: "883661844286",
+  appId: "1:883661844286:web:bef7e9676914d487b2466f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
